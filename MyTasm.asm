d266cc6f814fddcb0b9519c048a7f7503b1fb925
houidef <houidefaek@gmail.com>
2022-06-17 17:56:08 +0100

    update 17.06.2022 17:55

--- MyTasm.asm
+++ MyTasm.asm
@@ -53,12 +53,51 @@ extrn GetModuleFileNameA:PROC
 	BVar5 dd ?
 	BVar6 dd ?
 	BVar7 dd ?
-	proc3 dd ?
-	proc4 dd ?
+	BVar8 dd ?
+	BVar9 dd ?
+	BVar10 dd ?
+	BVar11 dd ?
+	BVar12 dd ?
+	BVar13 dd ?
+	BVar14 dd ?
+	BVar15 dd ?	
+	BVar16 dd ?
+	BVar17 dd ?
+	BVar18 dd ?
+	BVar19 dd ?
+	BVar20 dd ?
+	BVar21 dd ?
+	BVar22 dd ?
+	BVar23 dd ?
+	BVar24 dd ?
+	BVar25 dd ?
+	BVar26 dd ?
 	VNbrError2 dd ?
 	VNbrWarning2 dd ?
 	VNewLine dd ?
-	procBuild1 dd ?
+	VNbrError dd ?
+	VNbrWarning dd ?
+	MyPrintMsg dd ?
+	proc3 dd ?
+	proc4 dd ?
+	proc5 dd ?
+	proc6 dd ?
+	proc7 dd ?
+	proc8 dd ?
+	proc9 dd ?
+	proc10 dd ?
+	proc11 dd ?
+	proc12 dd ?
+	proc13 dd ?
+	proc14 dd ?
+	proc15 dd ?
+	MyFileAsm2 dd ?
+	MyFileObj dd ?
+	VNbrPassed2 dd ?
+	VNbrPassed1 dd ?
+    MyFileName dd ?
+	MyCreateObjFile dd ?
+	VTurboVarsion dd ?
 	align	4
 addition MACRO x,y
    mov eax, x 
@@ -141,7 +180,48 @@ InitVar proc
 	addition 1EB10h,proc4
 	;addition 1EBA1h,MyBuildingFile
 	addition 28944h,VNewLine
-	addition 1EBC1h,procBuild1
+
+	addition 2F28Eh,VNbrError
+	addition 2F290h,VNbrWarning
+	addition 2F7B6h,BVar10
+	addition 2F79Ch,BVar11
+	addition 1EA48h,proc6
+	addition 1EF1Dh,proc5
+	addition 1EED8h,proc7
+	addition 1EEE6h,proc8
+	addition 1EF06h,proc9
+	addition 2F299h,MyFileAsm2
+	addition 2F499h,BVar8
+	addition 2F599h,BVar9
+	addition 2F399h,MyFileObj
+	addition 2A7B4h,BVar22
+	addition 2A7C8h,BVar23
+	addition 2A7B6h,VNbrPassed2
+	addition 2F292h,VNbrPassed1
+	addition 2A7B8h,BVar24
+	addition 2A7C4h,BVar25
+	addition 2A7C2h,BVar26
+	addition 288C8h,VTurboVarsion
+	addition 1EA55h,proc10
+	addition 2F79Bh,BVar12
+	addition 2A7B5h,BVar13
+	addition 28927h,BVar16
+	addition 2F275h,BVar17
+	addition 2F294h,BVar18
+	addition 2F296h,BVar19
+	addition 289E9h,BVar20
+	addition 2F276h,BVar21
+	addition 42EC5Ch,BVar14
+	addition 2F295h,BVar15
+	addition 1EE13h,MyPrintMsg
+    addition 1EA6Fh,proc11
+	addition 1EA7Ch,proc12
+	addition 2F699h,MyFileName
+	addition 1EA62h,MyCreateObjFile
+	addition 099E3h,proc12
+	addition 0100Dh,proc13
+	addition 1E9A8h,proc14
+	
     pop eax	
 InitVar endp
 ;==================================================================================================
@@ -304,6 +384,189 @@ LMyBuildFile4:
 	jmp LMyBuildFile1 
 MyBuildFile endp
 ;==================================================================================================
-
+procBuild1 proc
+	push edi
+	push edi
+	push esi
+	xor eax,eax
+	load VNbrError
+	mov word ptr [ebx],ax
+	load VNbrWarning
+	mov word ptr [ebx],ax
+	mov eax,[MyFileAsm2]
+	load BVar10
+	mov dword ptr [ebx],eax
+	load BVar11
+	mov byte ptr [ebx],0h
+	load BVar2
+	and byte ptr [ebx],0FDh
+	call [proc6]
+	jae LprocBuild1 
+	pop esi
+	pop edi
+	push esi
+	call [proc5]
+	stc 
+	jmp LprocBuild2 
+LprocBuild1:
+	pop esi
+	mov edi,[MyFileAsm2]
+	call [proc7] 
+	mov edi,[MyFileObj]
+	call [proc7]  
+	mov edi,[BVar8]
+	call [proc7] 
+	mov edi,[BVar9]
+	call [proc7] 
+	push esi
+	mov esi,[MyFileAsm2]
+	call [proc8]
+	mov edi,[MyFileName]
+	call [proc7] 
+	mov esi,[MyFileName]
+	call [proc9]
+	mov byte ptr [esi],0h
+	pop esi
+	pop edi
+	push esi
+	mov ax,word ptr [edi+1h]
+	load BVar22
+	mov word ptr [ebx],ax
+	mov al,byte ptr [edi+3h]
+	load BVar23
+	mov byte ptr [ebx],al
+	mov ax,word ptr [edi+4h]
+	load VNbrPassed2
+	mov word ptr [ebx],ax
+	load VNbrPassed1
+	mov word ptr [ebx],ax
+	mov ax,word ptr [edi+6h]
+	load BVar24
+	mov word ptr [ebx],ax
+	mov ebx,dword ptr [edi+8h]
+	push ebx
+	load BVar25
+	mov word ptr [ebx],64h
+	load BVar26
+	mov word ptr [ebx],0FFFFh
+	load VTurboVarsion
+	mov edi,[ebx]
+    pop ebx	
+	mov ax,503h
+	call [proc10]
+	load BVar12
+	mov byte ptr [ebx],1h
+	;===================
+	jae LprocBuild3 
+	call [proc5] 
+	stc 
+	jmp LprocBuild4 
+LprocBuild3:
+    load BVar13
+	test byte ptr [ebx],2h
+	jne LprocBuild12 
+	call [MyPrintf]
+LprocBuild12:
+	pop esi
+	pop edi
+	push edi
+	push esi
+	lea esi,dword ptr [edi+14h]
+LprocBuild11:
+	cmp esi,dword ptr [edi+10h]
+	;=============================
+	jae LprocBuild10 
+	push edi
+	lodsb 
+	push eax
+	mov edi,[BVar14] 
+	mov edx,edi
+	exit
+	call [proc7]  
+	pop eax
+	push esi
+	cmp al,1h
+	je LprocBuild13 
+	mov byte ptr [BVar15],49h
+	call [proc11] 
+	jmp LprocBuild14 
+LprocBuild13:
+	mov byte ptr [BVar15],44h
+	mov edi,edx
+	call [proc12]
+LprocBuild14:
+    ;exit
+	pop esi
+	pop edi
+	jae LprocBuild11 
+	call [MyPrintMsg]
+	mov esi,[BVar16]
+	call [MyWriteToConsole] 
+	mov al,byte ptr [BVar17]
+	mov byte ptr [BVar18],al
+	mov byte ptr [BVar19],0h
+	mov esi,BVar18
+	call MyWriteToConsole 
+	mov esi,[BVar8]
+	call MyWriteToConsole 
+	mov esi,BVar20
+	call [MyWriteToConsole] 
+	call [proc5] 
+	mov esi,[VNewLine]
+	call [MyWriteToConsole] 
+	stc 
+	jmp LprocBuild4
+LprocBuild10:
+	mov byte ptr [BVar21],0h
+	mov ebx,[MyFileAsm2]
+	mov edx,[MyFileObj]
+	mov esi,[BVar8]
+	mov edi,[BVar9]
+	cmp byte ptr [esi],0h
+	;=========================================
+	jne LprocBuild8 
+	mov byte ptr [edi],0h
+LprocBuild8:
+	mov ecx,[MyFileName]
+	call [MyCreateObjFile] ;<==
+	exit
+	jae LprocBuild9 
+	mov byte ptr [BVar21],1h
+LprocBuild9:
+	call MyPrintMsg
+	cmp byte ptr [BVar21],1h
+	jne LprocBuild7 
+	call [MyPrintf]   
+	call [proc5] 
+	inc word ptr [BVar7]
+LprocBuild7:
+	test byte ptr [BVar2],2h
+	je LprocBuild6 
+	mov esi,[VNewLine]
+	call [MyWriteToConsole]
+LprocBuild6:
+	inc word ptr [BVar1]
+	mov ax,word ptr [VNbrError]
+	add word ptr [VNbrError2],ax
+	mov ax,word ptr [VNbrWarning]
+	add word ptr [VNbrWarning2],ax
+	clc 
+LprocBuild4:
+	pushfd 
+	exit
+	call [proc13]
+	mov byte ptr [BVar12],0h
+	call [proc14]
+	cmp byte ptr [BVar11],1h
+	jne LprocBuild5 
+	mov edx,MyFileObj
+	call [proc14]
+LprocBuild5:
+	popfd 
+LprocBuild2:
+	pop esi
+	pop edi
+	mov dword ptr [[BVar10]],0h
+endp
 end
 ;end start
